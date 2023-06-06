6829adce48fa273e3bf28bd8571857f3f7dc6be9
Elina Korotkova <128074533+ElinaKorotkova@users.noreply.github.com>
2023-06-06 22:11:52 +0500

    First commit
    First commit

---
+++ PP_2_2_2_spring-mvc-master_2_3_1_05_06/src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDAO;
+import web.entity.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements web.service.UserService {
+
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        if (user.getId() == 0) {
+            userDAO.saveUser(user);
+        } else {
+            updateUser(user);
+        }
+    }
+
+    @Override
+    @Transactional
+    public User getUser(long id) {
+        return userDAO.getUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(long id) {
+        userDAO.deleteUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        userDAO.updateUser(user);
+    }
+}
